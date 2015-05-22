6e47e141baadd9cd44dba38bd82b5c6c374d885a
Jugis <vargapalmark92@gmail.com>
2015-05-22 23:57:56 +0200

    Teszt 1-2-3

---
+++ Dummy1/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+	@Override
+	public String toString() {
+		return "Main []";
+	}
+	public static final Integer lofasz = 2;
+	private String cuccos;
+	List<String> asd = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+	public Main(String cuccos) {
+		super();
+		this.cuccos = cuccos;
+	}
+
+}
